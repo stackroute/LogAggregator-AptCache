049b1ac9d2949daf540213f98f80087e2cd38c10
tarun0409 <tarun.mohandas@gmail.com>
2015-12-28 15:49:13 +0530

    Implemented bootgrid

--- js/package_bz2_script.js
+++ js/package_bz2_script.js
@@ -1,47 +1,37 @@
 function WriteTable(data) {
+  head = $("<thead />")
+  $("#contenttable").append(head);
   var row = $("<tr />");
-  $("#contenttable").append(row);
-  row.append($("<th>Package Name</th>"));
-  row.append($("<th>Operating System</th>"));
-  row.append($("<th>Count</th>"));
+  head.append(row);
+  row.append("<th data-column-id='PackName'>Package Name</th>");
+  row.append("<th data-column-id='OSName'>Operating System</th>")
+  row.append("<th data-column-id='Count' data-order='desc' data-type='numeric'>Count</th>")
+  body = $("<tbody />")
+  $("#contenttable").append(body);
   for(ele in data)
   {
       WriteRow(data,ele);
   }
+  $("#contenttable").bootgrid();
 }
 
 function WriteRow(data,ele) {
-  var row = $("<tr />");
-  $("#contenttable").append(row);
-  var th = $('<th />')
-  th.append(ele);
-  row.append(th);
-  var i=0;
-  var flag = false;
   for(e in data[ele])
   {
-      if(flag === false)
-      {
-          row.append($("<td>"+e+"</td>"));
-          row.append($("<td>"+data[ele][e]["count"]+"</td>"));
-          flag = true;
-      }
-      else
-      {
-        var innerRow = $("<tr />");
-        $("#contenttable").append(innerRow);
-        innerRow.append($("<td>"+e+"</td>"));
-        innerRow.append($("<td>"+data[ele][e]["count"]+"</td>"));
-      }
-      i++;
+      var row = $("<tr />");
+      $(body).append(row);
+      row.append($("<td>"+ele+"</td>"));
+      row.append($("<td>"+e+"</td>"));
+      row.append($("<td>"+data[ele][e]["count"]+"</td>"));
 
   }
-  th.attr('rowspan',i);
-
+  //th.attr('rowspan',i);
 }
 
 $(document).ready(function(){
   $("#y2015").click(function(){
+    $("#contenttable").bootgrid("destroy");
+    $("#contenttable").empty();
       $("#dropdownMenu1").html("2015");
       $.ajax({
         url:'json/package_bz2_info/packages_all.json',
@@ -50,15 +40,18 @@ $(document).ready(function(){
         cache:false,
         success:function(data){
           WriteTable(data);
+
         }
       });
     });
   $('#y15').click(function(){
+
       $('#dropdownMenu2').html('2015');
       $("#monthList1 li").click(function() {
+        $("#contenttable").bootgrid("destroy");
+        $("#contenttable").empty();
           var month = $(this).text();
           var id = $(this).children().attr('id');
-          $("#contenttable").empty();
           $('#dropdownMenu3').html(month);
           $.ajax({
             url:'json/package_bz2_info/packages_monthly.json',
@@ -71,13 +64,15 @@ $(document).ready(function(){
           });
 
       });
-
     });
     $("#year_tab").click(function(){
-        $("#contenttable").empty();
+      $("#contenttable").bootgrid("destroy");
+      $("#contenttable").empty();
     });
     $("#month_tab").click(function(){
-        $("#contenttable").empty();
+      $("#contenttable").bootgrid("destroy");
+      $("#contenttable").empty();
     });
 
 });
+//$("#contenttable").bootgrid();
