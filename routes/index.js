c33f5106218d79bab41b6e8e22c413f11e2c8a6d
Ashish Gupta <ashishg251@gmail.com>
2016-01-11 12:06:13 +0530

    commiting

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var fs = require('fs');
+var app = express();
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home' ,navClass1:"active" ,navClass2:"" ,navClass3:"" ,navClass4:"" ,navClass5:"" ,navClass6:""});
 });
 
 router.get('/requestrate', function(req, res, next) {
-  res.render('allLogs', { title: 'All Logs' });
+  res.render('allLogs', { title: 'All Logs' ,navClass1:"" ,navClass2:"active" ,navClass3:"" ,navClass4:"" ,navClass5:"" ,navClass6:""});
 });
 
 router.get('/requestdata', function(req, res, next) {
-  res.render('allData', { title: 'All Data' });
+  res.render('allData', { title: 'All Data' ,navClass1:"" ,navClass2:"" ,navClass3:"active" ,navClass4:"" ,navClass5:"" ,navClass6:""});
 });
 
 router.get('/packagecount', function(req, res, next) {
-  res.render('package', { title: 'Package Count' });
+  res.render('package', { title: 'Package Count' ,navClass1:"" ,navClass2:"" ,navClass3:"" ,navClass4:"active" ,navClass5:"" ,navClass6:""});
 });
 
 router.get('/packageanalytics', function(req, res, next) {
-  res.render('package_bz2', { title: 'Package Analytics' });
+  res.render('package_bz2', { title: 'Package Analytics' ,navClass1:"" ,navClass2:"" ,navClass3:"" ,navClass4:"" ,navClass5:"active" ,navClass6:""});
 });
 
 router.get('/packagerepository', function(req, res, next) {
-  res.render('package_repo', { title: 'Package Repository' });
+  res.render('package_repo', { title: 'Package Repository' ,navClass1:"" ,navClass2:"" ,navClass3:"" ,navClass4:"" ,navClass5:"" ,navClass6:"active"});
 });
 
 router.get('/:charttype=?/:datename=?',function(req,res,next){
