a1cf772b9b0efb3528a3a5ced625134622ec07a3
Suriya <suriya.sivanathbabu@wipro.com>
2016-01-19 11:42:07 +0530

    Removed jquery files and fixed minor bugs

--- routes/index.js
+++ routes/index.js
@@ -9,24 +9,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-router.get('/requestrate', function(req, res, next) {
-  res.render('allLogs', { title: 'All Logs' });
-});
-
-router.get('/requestdata', function(req, res, next) {
-  res.render('allData', { title: 'All Data' });
-});
-
-router.get('/packagecount', function(req, res, next) {
-  res.render('package', { title: 'Package Count' });
-});
-
-router.get('/packageanalytics', function(req, res, next) {
-  res.render('packageAnalytics', { title: 'Package Analytics' });
-});
-
-router.get('/packagerepository', function(req, res, next) {
-  res.render('packageRepository', { title: 'Package Repository' });
-});
 
 module.exports = router;
