7e1fd3d9323d2f10cc8502bb63b461dd8da6a423
Bhairavi Balakrishnan <bhairavi.balakrishnan@wipro.com>
2016-01-21 12:37:16 +0530

    small changes

--- public/javascripts/angularScripts/dropdownModule.js
+++ public/javascripts/angularScripts/dropdownModule.js
@@ -1,12 +1,5 @@
 var dropdownModule=angular.module("DropDownModule",[]);
 
-dropdownModule.controller('DropDownController',function($scope)
-{
-  $scope.yearList=['2013','2014','2015','2016'];
-  // $scope.monthList=["January","February","March","April","May","June","July","August","September","October","November","December"];
-  $scope.monthList=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-});
-
 dropdownModule.directive('dropdownMonth',function(){
   return {
     restrict:'E',
@@ -18,5 +11,12 @@ dropdownModule.directive('dropdownYear', function(){
   return {
     restrict: 'E',
     templateUrl:'dropdownYear.html'
+  };
+});
+
+dropdownModule.directive('dropdownMode', function(){
+  return {
+    restrict: 'E',
+    templateUrl:'dropdownMode.html'
   };
 });
--- public/javascripts/angularScripts/logApp.js
+++ public/javascripts/angularScripts/logApp.js
@@ -1,5 +1,13 @@
 aptLogApp = angular.module("aptLogApp",['ngRoute','ui.grid','DropDownModule']);
 
+aptLogApp.controller('DropDownController',function($scope)
+{
+  $scope.yearList=['2013','2014','2015','2016'];
+  // $scope.monthList=["January","February","March","April","May","June","July","August","September","October","November","December"];
+  $scope.monthList=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  $scope.modeList=["Input","Output"];
+});
+
 aptLogApp.controller("ActiveController", function($scope){
   $scope.menuItems = [
     {
